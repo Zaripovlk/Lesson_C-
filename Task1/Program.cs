60910c8ecec64efbdc1ef27906ac21d5c4cec6b5
Zaripov Linar <Zaripovlk@gmail.com>
2023-12-20 21:28:06 +0300

    add some variables lesson1\task1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,3 +1,14 @@
 ﻿using System.Net.Security;
 
-Console.WriteLine("Hellow world");
+// Console.WriteLine("Hellow world");
+
+
+int value = 100;
+Console.WriteLine(value);
+
+double d = 5.55;
+char c = '!';
+bool b = true;
+Console.WriteLine(d);
+Console.WriteLine(c);
+Console.WriteLine(b);

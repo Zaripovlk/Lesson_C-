41aace4f2d91187d5d940c726d58ad6c74f453f7
Zaripovlk <zaripovlk@gmail.com>
2024-01-10 20:53:41 +0300

    add 3_4

---
+++ Lesson_3/3_4/Program.cs
@@ -0,0 +1,11 @@
+﻿int N = 5;
+int[] arr = { 9, 4, 7, 3, 9 };
+int i = 0;
+int max = arr[0];
+while (i < N)
+{
+    if (arr[i] > max)
+        max = arr[i];
+    i = i + 1;
+}
+Console.WriteLine(max);

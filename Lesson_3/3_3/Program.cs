f37ed7fb41733c0e2790ae20dc45ccce1cf67194
Zaripovlk <zaripovlk@gmail.com>
2024-01-10 20:44:16 +0300

    add 3_3

---
+++ Lesson_3/3_3/Program.cs
@@ -0,0 +1,14 @@
+﻿//int N = 10;
+int[] arr = { 2, 5, 4, 7, 8, 35, 1, 8, 5, 4 };
+int i = 0;
+
+while (i < arr.Length)
+{
+    if (arr[i] % 2 == 0)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    i = i + 1;
+
+}
+

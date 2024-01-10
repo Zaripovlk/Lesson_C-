2374356fd94621009a43c1be8f0171bffe91445e
Zaripovlk <zaripovlk@gmail.com>
2024-01-10 21:42:54 +0300

    add in 3_2 input console

--- Lesson_3/3_2/Program.cs
+++ Lesson_3/3_2/Program.cs
@@ -1,4 +1,4 @@
-﻿int N = 10;
+﻿/*int N = 10;
 int [] arr = new int[N];
 int i = 0;
 
@@ -12,4 +12,33 @@ while (i < arr.Length)
 
 
 }
+*/
+int n;
+Console.WriteLine("Введите число элементов массива");
+n = Convert.ToInt16(Console.ReadLine());
+// Объявим одномерный массив Data с n-элементами 
+double[] arr = new double[n];
+int i = 0;
+while (i < n)
+{
+    Console.WriteLine("Введите элемент массива");
+    /* Вводим элементы массива с клавиатуры 
+     * и заполняем ими массив */
+    arr[i] = double.Parse(Console.ReadLine());
+    Console.WriteLine();
+    i++;
+}
+// Вывод содержимого массива
+for (i = 0; i < arr.Length; i++)
+    Console.WriteLine("Элемент[" + i + "]: " + arr[i]);
 
+double max = arr[0];
+for (int j = 0; j < arr.Length; j++)
+{
+    if (arr[j] > max)
+    {
+        max = arr[j];
+    }
+
+}
+Console.WriteLine("Максимальное число массива: " + max + " ");

f5e5d87a62a45fd2c8d84364df87d717fa3b3c72
Zaripovlk <zaripovlk@gmail.com>
2024-01-16 23:29:15 +0300

    add L4_1

---
+++ Lesson_4/L4_1/Program.cs
@@ -0,0 +1,10 @@
+﻿double calculateFormula(int a, int b, int c, int d)
+{
+    double numenator = a * b;
+    int denomenator = c + d;
+    double result = numenator / denomenator;
+    return result;
+}
+double result = calculateFormula(1, 2, 3, 4);
+
+Console.WriteLine(result);

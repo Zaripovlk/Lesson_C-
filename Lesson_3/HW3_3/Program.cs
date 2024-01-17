8f14d2f9379d0993b4f04316f88d4b0e2e5e638d
Zaripovlk <zaripovlk@gmail.com>
2024-01-17 19:18:13 +0300

    add HW3_3

---
+++ Lesson_3/HW3_3/Program.cs
@@ -0,0 +1,43 @@
+﻿/*Задайте массив из вещественных чисел с ненулевой дробной частью. 
+Найдите разницу между максимальным и минимальным элементов массива
+*/
+void Print(double[] arr)
+{
+    int size = arr.Length;
+    for (int i = 0; i < size; i++)
+    {
+        Console.WriteLine($"{arr[i]} ");
+    }
+    Console.WriteLine(); //разделение строкой
+}
+double[] MassNums(int size, int from, int to)
+{
+    double[] arr = new double[size];
+    Random n_new = new Random();
+
+    for (int i = 0; i < size; i++)
+        arr[i] = Math.Round(n_new.NextDouble() * (to - from) + from, 2);
+    return arr;
+}
+void DiffMaxMin(double[] arr)
+{
+    double n_max = arr[0];
+    double n_min = arr[0];
+    for (int i = 1; i < arr.Length; i++)
+    {
+        if (n_max < arr[i])
+            n_max = arr[i];
+        else if (n_min > arr[i])
+            n_min = arr[i];
+    }
+    Console.Write($"Max: {n_max}, min: {n_min}. ");
+    Console.WriteLine($"Difference: {n_max} - ({n_min}) = {Math.Round(n_max - n_min)}");
+}
+
+// 10, 10 , 12
+int num = int.Parse(Console.ReadLine()!);
+int start = int.Parse(Console.ReadLine()!);
+int stop = int.Parse(Console.ReadLine()!);
+double[] mass = MassNums(num, start, stop);
+Print(mass);
+DiffMaxMin(mass);

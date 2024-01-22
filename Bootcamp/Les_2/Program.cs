671fe5cdfc9514275eea174bbbbd52fd8d770eda
Zaripovlk <zaripovlk@gmail.com>
2024-01-22 22:59:09 +0300

    add bootcamp

---
+++ Bootcamp/Les_2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+int[] array = new int[5];
+int conv = 0;
+for (int i = 0; i < 5; i++)
+
+{
+    conv = i + 1;
+    Console.Write($"Введите число {conv}: ");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine("Созданный массив: [" + string.Join(",", array) + "]");
+Console.WriteLine("Выводим 4 элемент массива: " + array[3]);
+
+// Сложность алгоритма О(1)- это количество действий чтобы узнать результат.

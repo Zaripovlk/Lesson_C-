814dc07fab0dfb8a06fe059f7341b5a385d2e51a
Zaripovlk <zaripovlk@gmail.com>
2024-01-28 19:27:16 +0300

    add L5_1

---
+++ Lesson_5/L5_1/Program.cs
@@ -0,0 +1,35 @@
+﻿
+
+int[,] CreatMatrix(int rowcount, int columsCount)
+{
+    int[,] matrix = new int[rowcount, columsCount];
+
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+
+            matrix[i, j] = rnd.Next(1, 11);
+
+        }
+    }
+    return matrix;
+}
+void ShowMatrix(int[,] matrix)
+{
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+
+        Console.Write($"{matrix[i, j]} "); // интерпаляция строк
+
+    }
+    Console.WriteLine();
+
+}
+}
+
+int [,] matrix = CreatMatrix(4,5);
+ShowMatrix(matrix);

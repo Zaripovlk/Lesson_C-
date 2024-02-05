9caf31574088ecdec726869b3132823fc7b0adbd
Zaripovlk <zaripovlk@gmail.com>
2024-02-05 16:59:38 +0300

    add HW6_2

---
+++ Lesson_6/HW6_2/Program.cs
@@ -0,0 +1,13 @@
+﻿/* Задайте строку, содержащую латинские буквы в обоих регистрах. 
+Сформируйте строку, в которой все заглавные буквы заменены на строчные.*/
+Console.Clear();
+
+string Stroka(string word)
+{
+    return word.ToLower();
+}
+Console.WriteLine("Enter some text: ");
+string text = Console.ReadLine()!;
+
+string result = Stroka(text);
+Console.WriteLine(result);

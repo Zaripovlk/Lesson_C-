abe227f236e90e9276efab8fed1e12445a51153a
Zaripovlk <zaripovlk@gmail.com>
2024-02-05 17:13:28 +0300

    add HW6_3

--- Lesson_6/HW6_3/Program.cs
+++ Lesson_6/HW6_3/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задайте произвольную строку. Выясните, является ли она палиндромом.*/
+Console.Clear();
+
+bool Polin(string word)
+{
+    int size = word.Length;
+    for (int i = 0; i < size; i++)
+    {
+        if (word[i] ! == word[size - 1 - i])
+            return false;
+    }
+    return true;
+}
+
+Console.Write("Введите текст: ");
+string text = Console.ReadLine()!;
+
+bool result = Polin(text);
+Console.WriteLine($"Строка полиндром - {result}");
